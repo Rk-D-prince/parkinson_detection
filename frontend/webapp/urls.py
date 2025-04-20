65921f31e4ce4a15eb411fa756221239042b7a44
Rk-D-prince <126610387+Rk-D-prince@users.noreply.github.com>
2025-04-20 16:38:34 +0530

    Delete frontend directory

--- frontend/webapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .import views
-urlpatterns = [
-    path('',views.home,name='home'),
-    path('input',views.input,name='input'),
-    path('output',views.output,name='output')
-]
